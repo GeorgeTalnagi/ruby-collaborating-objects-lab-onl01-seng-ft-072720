f87b49ebec45516e7f66292e609153f38f5ae9e0
George Talnagi <gtalnagi@yahoo.com>
2020-08-12 04:45:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ attr_accessor :name, :artist
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
     new_song = self.new(song)
-    artist_name=(artist)
+    new_song.artist_name=(artist)
     new_song
     end
      
