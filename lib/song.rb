7559b35297e98af9d7a8a5285484a4e1c26f3d2a
George Talnagi <gtalnagi@yahoo.com>
2020-08-12 04:17:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,11 @@ attr_accessor :name, :artist_name
     new_song = self.new(song)
     new_song.artist_name = Artist.find_or_create_by_name(artist)
     new_song
-    
+    end
+     
+     def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
    
 
